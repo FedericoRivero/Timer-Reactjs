edb812762a0b54156f2b482f78c697a45b50bf35
Fede Rivero <federicorivero18@gmail.com>
2023-01-04 03:37:05 -0300

    updated timer component to use current year for deadline

--- src/Timer.js
+++ src/Timer.js
@@ -2,28 +2,30 @@ import React from "react";
 import { useState, useEffect } from "react";
 import logo from "./sand-clock.svg";
 
+const currentYear = new Date().getFullYear();
+
 const Timer = () => {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
 
-  const deadline = "December, 31, 2022";
-
-  const getTime = () => {
-    const time = Date.parse(deadline) - Date.now();
-
-    setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
-    setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
-    setMinutes(Math.floor((time / 1000 / 60) % 60));
-    setSeconds(Math.floor((time / 1000) % 60));
-  };
+  const deadline = `December, 31, ${currentYear}`;
 
   useEffect(() => {
-    const interval = setInterval(() => getTime(deadline), 1000);
+    const getTime = () => {
+      const time = Date.parse(deadline) - Date.now();
+  
+      setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+      setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+      setMinutes(Math.floor((time / 1000 / 60) % 60));
+      setSeconds(Math.floor((time / 1000) % 60));
+    };
 
-    return () => clearInterval(interval);
-  }, []);
+  const interval = setInterval(() => getTime(deadline), 1000);
+
+  return () => clearInterval(interval);
+}, [deadline]);
 
   return (
     <div className="timer">
